ee997e4aac6f79fd3255f5b073c30edb22fe0989
yulishanngrum <yulishacahya@gmail.com>
2020-05-03 21:58:42 +0700

    2

--- app/src/main/java/com/example/kamusnetbeans/MainActivity.java
+++ app/src/main/java/com/example/kamusnetbeans/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.kamusnetbeans;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    EditText netbeans, arti;
+    private Button tombol; // Membuat Variable Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        netbeans = (EditText) findViewById(R.id.txtnetbeans);
+        arti = (EditText) findViewById(R.id.txtarti);
+        tombol = findViewById(R.id.btntranslate);
+
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(netbeans.getText().toString().equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Data Kosong, Silahkan Masukan Data Terlebih Dahulu..", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(netbeans.getText().toString().equals("New Project"))
+                    {
+                        arti.setText("membuat sebuah rancangan baru yang berisi kumpulan berkas pemrograman");
+                    }
+                    else if(netbeans.getText().toString().equals("New file"))
+                    {
+                        arti.setText("membuat sebuah berkas pemrograman");
+                    }
+                    else if(netbeans.getText().toString().equals("Open Project"))
+                    {
+                        arti.setText("membukan rancangan yang sudah ada");
+                    }
+                    else if(netbeans.getText().toString().equals("Open recent project"))
+                    {
+                        arti.setText("membuka rancangan yang sudah dibuka akhir-akhir ini");
+                    }
+                    else if(netbeans.getText().toString().equals("Close Project"))
+                    {
+                        arti.setText("menutup rancangan yang sudah ada");
+                    }
+                    else if(netbeans.getText().toString().equals("Close Other Project"))
+                    {
+                        arti.setText("menutup rancangan lainnya");
+                    }
+                    else if(netbeans.getText().toString().equals("Close All Project"))
+                    {
+                        arti.setText("menutup semua rancangan");
+                    }
+                    else if(netbeans.getText().toString().equals("Open File"))
+                    {
+                        arti.setText("membukan berkas yang sudah ada");
+                    }
+                    else if(netbeans.getText().toString().equals("Project Group"))
+                    {
+                        arti.setText("kumpulan rancangan yang sudah ataupun akan dijadikan satu kesatuan");
+                    }
+                    else if(netbeans.getText().toString().equals("Import Project"))
+                    {
+                        arti.setText("untuk mengmpor sebuah rancangan yang sudah dibuat");
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Data Belum Terdaftar..", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
